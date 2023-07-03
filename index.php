075846d837db883c404485a2fc024be3bbf4efbb
LisaKoizumiCipolla <cipollalisa17@gmail.com>
2023-07-03 18:16:46 +0200

    Minor adjustment

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"> 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-          <title>BOOTSTRAP DASHBOARD</title>
+          <title>HOTEL</title>
 </head>
 <body>
     
@@ -62,7 +62,7 @@
                     <tr>
                         <?php foreach (array_keys($firstHotel) as $hotelKey) { ?>
                             <th scope="col">
-                                <?php echo $hotelKey; ?>
+                                <?php echo strtoupper($hotelKey); ?>
                             </th>
                         <?php } ?>
                     </tr>
@@ -91,7 +91,7 @@
                         <?php echo $hotel['vote']; ?>
                     </td>
                     <td>
-                        <?php echo $hotel['distance_to_center']; ?>
+                        <?php echo $hotel['distance_to_center']; ?> km
                     </td>
                 </tr>
                 
