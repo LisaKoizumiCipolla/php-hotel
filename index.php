39be260d38d4277c7b339d0c6ea1e1990f1a3b79
LisaKoizumiCipolla <cipollalisa17@gmail.com>
2023-07-03 16:43:18 +0200

    Table done (problem printing key)

--- index.php
+++ index.php
@@ -45,36 +45,58 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-    <title>HOTEL LIST</title>
+    
+<meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"> 
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+          <title>BOOTSTRAP DASHBOARD</title>
 </head>
 <body>
-    <h1>HOTEL LIST</h1>
-    <ul>
-        <?php
-        foreach ($hotels as $hotel)
-        { ?>
+    
+    <table class="table">
+            <thead>
+                    <tr>
+                        <?php foreach ($hotels as $hotel => $random) {
+                            ?>
+                            <th scope="col">
+                            <?php echo print_r(array_keys($hotel)); ?>
+                            </th>
+                        <?php } ?>
+                    </tr>
+            </thead>
+            <tbody>
+                <?php
+                    foreach ($hotels as $hotel) {
+                ?>
+                <tr>
+                    <th scope="row">
+                        <?php echo $hotel['name']; ?>
+                    </th>
+                    <td>
+                        <?php echo $hotel['description']; ?>
+                    </td>
+                    <td>
+                        <?php 
+                            if (!$hotel['parking']){
+                                echo "No parking";
+                            } else {
+                                echo "Yes";
+                            } 
+                        ?>
+                    </td>
+                    <td>
+                        <?php echo $hotel['vote']; ?>
+                    </td>
+                    <td>
+                        <?php echo $hotel['distance_to_center']; ?>
+                    </td>
+                </tr>
+                
+                <?php } ?>
+            </tbody>
+    </table>
 
-        <li>
-            <h2>
-                <?php echo $hotel['name']; ?>
-            </h2>
-            <?php echo $hotel['description']; ?>
-            <?php echo $hotel['parking']; ?>
-            <?php 
-                if (!$hotel['parking']){
-                    echo "No parking";
-                } else {
-                    echo "Yes";
-                } 
-            ?>
-            <?php echo $hotel['distance_to_center']; ?>
-        </li>
-
-        <?php 
-        } ?>
-    </ul>
 </body>
 </html>
